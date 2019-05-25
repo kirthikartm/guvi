0479f2615971fc6e5109622de701acbbe2a74b74
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-25 10:21:19 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,2 +1,8 @@
-a=input("enter the string")
-print(s1.isalpha())
+ab=input("")
+if(ab.isalpha()):
+    if(ab=='a' or ab=='e' or ab=='i' or ab=='o' or ab=='u'):
+      print("vowles")
+    else:
+      print("Consonant")
+
+
