38594aa755c57e649338199eef678f4c951830dc
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-25 11:35:49 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,9 +1,8 @@
-a=int(input(""))
-b=int(input(""))
-c=int(input(""))
+a,b,c=[int(a) for a in input("").split()]
 if(a>b and a>c):
     print(a)
 elif(b>a and b>c):
     print(b)
 elif(c>a and c>b):
     print(c)
+
