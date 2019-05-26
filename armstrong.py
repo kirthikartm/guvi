88ebe1d8d049a000f900228c8b63ab1f7a5153eb
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-26 13:17:34 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,11 @@
+a,b=map(int,input("").split())
+for i in range(a,b):
+    s=0
+    l=len(str(i))
+    t=i
+    while(t>0):
+        x=t%10
+        s=s+(x**l)
+        t=t//10
+    if(s==i):
+        print(i)
