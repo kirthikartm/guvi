f665b0b7205e4e1dcd1fdd77b8db8a216301892f
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-26 13:43:22 +0530

    Create prime1.py

---
+++ prime1.py
@@ -0,0 +1,8 @@
+a,b=map(int,input("").split())
+for i in range(a+1,b):
+    if(a>1):
+        for j in range(2,i):
+            if i%j==0:
+                break
+        else:
+            print(i,end=" ")
