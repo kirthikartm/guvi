932fd7c740bb7f14dadf73296bb1cf2a3a5cc073
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-27 22:16:35 +0530

    Update greatest_10.py

--- greatest_10.py
+++ greatest_10.py
@@ -1,10 +1,6 @@
 a=int(input(""))
 x=[int(a) for a in input("").split(" ")]
-l=len(x)
-max=0
-for i in range(l):
-        if(x[i]>max):
-            max=x[i]
-print(max)
+y=max(x)
+print(y)
         
     
