e0dbf91b153acedf23490223dcf2028d70e514e3
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-05-31 21:21:14 +0530

    Update reverse_word_hunter.py

--- reverse_word_hunter.py
+++ reverse_word_hunter.py
@@ -1,2 +1,11 @@
-a,b=map(str,input("").split(" "))
-print(a[::-1]+" "+(b[::-1]))
+a=input()
+a=a.split()
+l=[]
+for i in a:
+    l.append(i)
+x=""
+for i in range(0,len(l)):
+    x=l[i]
+    l[i]=x[::-1]
+for i in l:
+    print(i,end=" ")
