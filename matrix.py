7866d6797daea4071c144c2ab39f7cb18c808c4d
kirthikartm <50898007+kirthikartm@users.noreply.github.com>
2019-06-11 20:02:36 +0530

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,19 @@
+a,b=map(int,input().split())
+x=[]
+l1=[]
+for i in range(a):
+    l=[int(x) for x in input().split()]
+    x.append(l)
+    if 0 in l:
+        m=l.index(0)
+        l1.append(m)
+print(l1)
+for i in range(len(x)):
+    if 0 in x[i]:
+        for j in range(b):
+            x[i][j]=0
+for i in l1:
+    for j in range(a):
+        x[j][i]=0
+for i in x:
+    print(*i)
